56e88b1993a90f2ea37cd36cec78660db3a83cb9
Yaraib Arif <yarbahadar52@gmail.com>
2025-08-05 11:32:45 -0700

    To lead

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import '../providers/note_provider.dart';
+import '../utils/shared_preferences.dart';
 import 'add_note_screen.dart';
 import 'edit_note_screen.dart';
 import '../widgets/note_card.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime? _lastOpened;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadLastOpened();
+  }
+
+  void _loadLastOpened() {
+    setState(() {
+      _lastOpened = SharedPref.getLastopened();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +36,7 @@ class HomeScreen extends StatelessWidget {
     final notes = noteProvider.notes;
 
     return Scaffold(
-      backgroundColor: const Color(0xFFF3F6FD), // 💠 Light background
+      backgroundColor: const Color(0xFFF3F6FD),
       appBar: AppBar(
         backgroundColor: Colors.indigo,
         elevation: 0,
@@ -30,55 +51,68 @@ class HomeScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: notes.isEmpty
-          ? const Center(
-        child: Text(
-          '📝 No notes yet.\nTap + to add your first note!',
-          style: TextStyle(
-            fontSize: 16,
-            color: Colors.grey,
-          ),
-          textAlign: TextAlign.center,
-        ),
-      )
-       :ListView.builder(
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        itemCount: notes.length,
-        itemBuilder: (context, index) {
-          final note = notes[index];
-
-          return Dismissible(
-            key: ValueKey(note.timestamp),
-            direction: DismissDirection.endToStart,
-            background: Container(
-              color: Colors.red,
-              alignment: Alignment.centerRight,
-              padding: const EdgeInsets.only(right: 20),
-              child: const Icon(Icons.delete, color: Colors.white),
+      body: Column(
+        children: [
+          if (_lastOpened != null)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '📅 Last Opened: ${DateFormat('dd MMM yyyy – hh:mm a').format(_lastOpened!)}',
+                style: const TextStyle(fontSize: 14, color: Colors.grey),
+              ),
             ),
-            onDismissed: (_) {
-              Provider.of<NoteProvider>(context, listen: false).deleteNote(index);
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('🗑️ Note Deleted')),
-              );
-            },
-
-            child: NoteCard(
-              note: note,
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => EditNoteScreen(
-                      note: note,
-                      index: index,
-                    ),
+          Expanded(
+            child: notes.isEmpty
+                ? const Center(
+              child: Text(
+                '📝 No notes yet.\nTap + to add your first note!',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            )
+                : ListView.builder(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                final note = notes[index];
+                return Dismissible(
+                  key: ValueKey(note.timestamp),
+                  direction: DismissDirection.endToStart,
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerRight,
+                    padding: const EdgeInsets.only(right: 20),
+                    child: const Icon(Icons.delete, color: Colors.white),
+                  ),
+                  onDismissed: (_) {
+                    Provider.of<NoteProvider>(context, listen: false)
+                        .deleteNote(index);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('🗑️ Note Deleted')),
+                    );
+                  },
+                  child: NoteCard(
+                    note: note,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => EditNoteScreen(
+                            note: note,
+                            index: index,
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 );
               },
             ),
-          );
-        },
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.indigo,
@@ -88,7 +122,7 @@ class HomeScreen extends StatelessWidget {
             MaterialPageRoute(builder: (_) => const AddNoteScreen()),
           );
         },
-        child: const Icon(Icons.add, size: 28,color: Colors.white,),
+        child: const Icon(Icons.add, size: 28, color: Colors.white),
       ),
     );
   }
